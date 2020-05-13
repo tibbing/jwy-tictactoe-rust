f02be54f1a342fb94ef5de014bdea1ed1984f60b
Adam Tibbing <adam@tibbing.com>
2020-05-12 19:36:46 -0700

    Position input

--- tictactoe/src/main.rs
+++ tictactoe/src/main.rs
@@ -1,14 +1,41 @@
+use ::std::*;
+
 fn main() {
   println!("Starting new game...");
-
   let points = Vec::new();
   let mut _board: Board = Board { points };
 
-  _board = place(_board, Point {x:0, y:0, player:Player::Player1});
-  _board = place(_board, Point {x:0, y:1, player:Player::Player2});
-  _board = place(_board, Point {x:1, y:1, player:Player::Player1});
-  _board = place(_board, Point {x:1, y:0, player:Player::Player2});
-  _board = place(_board, Point {x:0, y:2, player:Player::Player2});
+  let mut count = 0u32;
+  let mut player:Player = Player::Player1;
+  println!("{} turn:",player.as_str());
+
+  loop {
+    count += 1;
+
+    let mut input = String::new();
+    match io::stdin().read_line(&mut input) {
+      Ok(_n) => {
+          println!("Placing marker at {}...", input);
+          let pos: Vec<_> = input.trim()
+            .split_whitespace()
+            .map(|x| x.parse::<i32>().expect("Not an Integer!"))
+            .collect();
+
+          let _pos_x: i32 = pos[0];
+          let _pos_y: i32 = pos[1];
+          _board = place(_board, Point {x:_pos_x, y:_pos_y, player});
+
+          if let Player::Player1 = player { player = Player::Player2 } else { player = Player::Player1 }
+          println!("{} turn:",player.as_str());
+        }
+      Err(error) => println!("error: {}", error),
+    }
+
+    if count == 3 {
+        println!("OK, that's enough");
+        break;
+    }
+  }
 
   show(_board);
   println!("Game over!");
@@ -38,7 +65,7 @@ impl Player {
         &Player::Player1 => "X",
         &Player::Player2 => "O",
     }
-}
+  }
 }
 
 #[derive(Copy, Clone, Debug)]
